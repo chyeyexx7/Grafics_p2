974136d1f3e16cbfd918384df623d9fb88172123
Chang <1020994740a@gmail.com>
2022-05-23 20:29:35 +0200

    Fase 2 siluetas

--- resources/toon_fshader.glsl
+++ resources/toon_fshader.glsl
@@ -16,84 +16,34 @@ struct lightsGpu
     vec4 lightPosition_gpu;
     vec4 lightDirection_gpu;
 };
+
 uniform lightsGpu lights[1];
-
-//Global light
-uniform vec3 ambientGlobalLight;
-
-//Struct material
-struct mtr{
-    vec3 Kd;
-    vec3 Ka;
-    vec3 Ks;
-    float shininess;
-    float opacity;
-};
-uniform mtr material;
 
 //Posición del observador
 uniform vec4 obs;
 
 void main()
 {
-    vec3 Itotal = ambientGlobalLight * material.Ka;
-    vec3 idkd;
-    vec3 isks;
-    vec3 iaka;
+    float intensity, silhouette;
+    vec4 color;
 
-    //N es la normal al punto normalizada
-    vec4 N = normal;
-    //V es el vector normalizado entre el observador y el punto
-    vec4 V = normalize(obs - position);
-    //L es el vector normalizado entre la luz y el punto
-    //Su calculo varia segun el tipo de luz
-    vec4 L;
-    //H se calcula a partir de N,L y V
-    vec4 H;
-    float toon;
-    float factorToon = 0.05;
-    vec4 direction;
-    float a, b, c, distance, attenuation;
+    // Calculamos la intensidad del toon entre 4 intervalos
+    intensity = dot(lights[0].lightDirection_gpu, normal);
+    if (intensity > 0.95)
+        color = vec4(1.0, 0.5, 0.5, 1.0);
+    else if (intensity > 0.5)
+        color = vec4(0.6, 0.3, 0.3, 1.0);
+    else if (intensity > 0.25)
+        color = vec4(0.4, 0.2, 0.2, 1.0);
+    else
+        color = vec4(0.2, 0.1 ,0.1, 1.0);
 
-    //Vamos a ir calculando phong para cada luz segun su tipo {Puntual, Direccional, Spot}
-    for(int i=0; i<lights.length(); i++){
-        //Luz puntual
-        if(lights[i].lightType_gpu == 0){
-            L = normalize(lights[i].lightPosition_gpu - position) ;
-            //Calculamos el valor de la distancia
-            distance = length(L);
-            //Nos guardamos cada uno de los coeficientes a,b,c
-            a = lights[i].coeficients_gpu.x;
-            b = lights[i].coeficients_gpu.y;
-            c = lights[i].coeficients_gpu.z;
-            //Calculo de la atenuación
-            attenuation = a*distance*distance + b*distance + c;
-        }
+    // Calculamos la silueta con la fórmula cos(alfa), donde alfa es el producto escalar entre normal y vector visión.
+    // Si alfa = 0 tenemos un borde.
+    silhouette = dot(normal, normalize(obs - position));
+    //if (silhouette < 0.1)
+      //  color *= silhouette;
 
-        //Luz direccional (no tenemos posición, solo dirección)
-        else if(lights[i].lightType_gpu == 1){
-            L = normalize(-lights[i].lightDirection_gpu);
-            attenuation = 1.0;
-        }
-        toon = dot(L,N);
-
-        if(toon>=0.25 && toon<=0.5 ){
-            factorToon = 0.25;
-        }else if(toon<0.75){
-            factorToon = 0.50;
-        }else if(toon >= 0.75){
-            factorToon = 0.75;
-        }
-
-        H = normalize(L+V);
-        idkd = lights[i].lightID_gpu * material.Kd * max(dot(N,L), 0.0);
-        isks = lights[i].lightIS_gpu * material.Ks * pow(max(dot(N,H), 0.0), material.shininess);
-        iaka = lights[i].lightIA_gpu * material.Ka;
-        //Para hacer pruebas con los ejemplos del campus hemos quitado la atenuación de la fórmula de phong
-        //Itotal += ((idkd + isks)/attenuation) + iaka;
-        Itotal += ((idkd * factorToon + isks)/attenuation) + iaka;
-    }
-    //El color de salida será el calculado con la fórmula de phong
-    colorOut = vec4(Itotal,1.0);
+    colorOut = color;
 }
 
